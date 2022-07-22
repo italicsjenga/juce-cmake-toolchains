0a2cb5f6e6c11bcec4aa6fc9030d39d05dd2eb72
Alex Janka <alex@alexjanka.com>
2022-07-22 16:59:43 +1000

    only existing toolchains in build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 if [ -z $1 ]; then
 	echo "No toolchain specified!"
-	return
+	exit
+fi
+if [ ! -f toolchain-$1.cmake ]; then
+	echo "file doesntnt exist!"
+	exit
 fi
 cd source
-cmake -B cmake-build-install-$1 -DCMAKE_INSTALL_PREFIX=../$1 -DCMAKE_TOOLCHAIN_FILE=../toolchain-$1.cmake && cmake --build cmake-build-install-$1 --target install
+cmake -B build-install-$1 -DCMAKE_INSTALL_PREFIX=../$1 -DCMAKE_TOOLCHAIN_FILE=../toolchain-$1.cmake && echo "--- Finished configuring ---" && cmake --build build-install-$1 --target install
